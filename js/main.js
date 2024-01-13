c3ea85371d282048e3d3c4c4901c1ec4ce073892
Faruks23 <mdfaruksp2@gmail.com>
2024-01-13 14:20:16 +0600

    create all section

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+// const swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//     1024: {
+//       slidesPerView: 5,
+//       spaceBetween: 50,
+//     },
+//   },
+// });
+
+
+
+
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
