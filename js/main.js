794a66101b76f43c352777f3b1d3ca6c637add21
Faruks23 <mdfaruksp2@gmail.com>
2024-01-14 18:08:40 +0600

    add tablet reponsive

--- js/main.js
+++ js/main.js
@@ -22,19 +22,54 @@
 //     },
 //   },
 // });
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".custom-button-next",
+      prevEl: ".custom-button-prev",
+    },
+    on: {
+      slideChange: function () {
+        updateCounter();
+      },
+    },
+    breakpoints: {
+      // When window width is <= 576px
+      576: {
+        slidesPerView: 1,
+      },
+      // When window width is <= 768px
+      768: {
+        slidesPerView: 2,
+      },
+      // When window width is <= 992px
+      992: {
+        slidesPerView: 3,
+      },
+    },
+  });
+ // Add event listeners for custom buttons
+ document
+   .querySelector(".custom-button-next")
+   .addEventListener("click", function () {
+     swiper.slideNext();
+   });
+
+ document
+   .querySelector(".custom-button-prev")
+   .addEventListener("click", function () {
+     swiper.slidePrev();
+   });
+
+    // Function to update the counter
+  function updateCounter() {
+    var currentSlide = swiper.realIndex + 1;
+    var totalSlides = swiper.slides.length;
+    document.querySelector('.swiper-counter').textContent = currentSlide + ' / ' + totalSlides;
+  }
+
+  // Initial update of the counter
+  updateCounter();
 
 
-
-
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
